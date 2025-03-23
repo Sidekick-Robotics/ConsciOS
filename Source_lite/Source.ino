f5c1c8df5bad260051b2f0df1d99eaf500fbf4fc
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2025-03-23 15:27:12 +0000

    Update Source.ino
    Added information to the lite template

--- Source_lite/Source.ino
+++ Source_lite/Source.ino
@@ -1,5 +1,24 @@
-\\ include goose to use the sight app
+// include goose to use the sight app
 #include "goose.h"
+
+// includes go here
+
+/*
+*    Functions from the goose file to make full use of the Sight app:
+*
+*    PRINT("your string");        // Normal print statement (serial.print also works)
+*    GRAPH("label", data, TOP);   // Add data to the top graph
+*    GRAPH("label", data, BOT);   // Add data to the bottom graph
+*
+*    START_REC;                   // Start recording on sight
+*    END_REC;                     // End recording on sight
+*
+*    END_LOG;                     // Stop the data logging (this is the same as println)
+*
+*    For more go to: https://github.com/Sidekick-Robotics/Sight/blob/main/README.md
+*/
+
+// Definitions go here
 
 void setup()
 {
@@ -9,4 +28,5 @@ void setup()
 void loop()
 {
     // Code to loop goes here.
+    END_LOG;
 }
